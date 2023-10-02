4496de72812c27917f09e784ee665948e3b2ca4f
Manoj Kumar <manojkp124@gmail.com>
2023-10-02 20:35:52 +0530

    Add Ciurcuit Breaker - Resilience4j in order-service and actuator for fault tolerance and service-helth

--- order-service/src/main/java/com/kumarmanoj/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/kumarmanoj/orderservice/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.kumarmanoj.orderservice.controller;
 
 import com.kumarmanoj.orderservice.dto.OrderRequest;
 import com.kumarmanoj.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +21,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops something went wrong, please order after some time!";
     }
 }
